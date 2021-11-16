ebd8845eaa23d867bd814a1ddb72e19e8008a1cf
FLU <valerio.massj@gmail.com>
2021-11-16 12:54:04 +0100

    send message functon and answer "ok" function + search contact

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 Milestone 1
 ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse.
 ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
+Milestone2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione 
+● Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone3
+● Aggiunta di un messaggio : l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde 
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+Milestone5-opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato 
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+
 */
 
 var app = new Vue (
@@ -10,6 +26,7 @@ var app = new Vue (
         data: {
             activeContact: 0,
             newMessage: "",
+            searchedValue: "",
             contacts: [
                 { 
                     name:   'Michele',
@@ -97,16 +114,30 @@ var app = new Vue (
             ] 
         },
         methods: {
-/*             getNewMessage(){
+            pushNewMessage(){
                 let newMsg = {
 
                     date:   '10/01/2020   15:30:55', 
-                    text:   newMessage, 
+                    text:   this.newMessage, 
                     status:   'sent'
 
                 }
-                this.contacts[0].messages.push(newMsg);
-            } v-model="newMessage" @keydown.enter="getNewMessage()"*/
+        
+                this.contacts[this.activeContact].messages.push(newMsg);
+                this.newMessage = "";
+            },
+            sendAnswer(){
+                setTimeout(() => {
+                    let newMsg = {
+
+                        date:   '10/01/2020   15:30:55', 
+                        text:   "ok", 
+                        status:   'received'
+    
+                    }
+                    this.contacts[this.activeContact].messages.push(newMsg);
+                }, 1000);
+            }
         }
     }
 )
