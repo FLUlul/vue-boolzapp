707acbc05e1c67e20bcb2bcb975ea9a0d73ac1f0
FLU <valerio.massj@gmail.com>
2021-11-17 10:41:58 +0100

    added .slice to cut the last message in contacts section

--- js/script.js
+++ js/script.js
@@ -37,17 +37,17 @@ var app = new Vue (
                     messages: [
                             {
                                 date:   '10/01/2020   15:30:55',
-                                text:   'Hai   portato   a   spasso   il    cane?',
+                                text:   'Hai portato a spasso il cane?',
                                 status:   'sent'
                             },
                             { 
                                 date:   '10/01/2020   15:50:00',
-                                text:   'Ricordati   di    dargli   da    mangiare',
+                                text:   'Ricordati di dargli da mangiare',
                                 status:   'sent'
                             },
                             { 
                                 date:   '10/01/2020   16:15:22',
-                                text:   'Tutto   fatto!',
+                                text:   'Tutto fatto!',
                                 status:   'received'
                             }
                     ],
@@ -59,17 +59,17 @@ var app = new Vue (
                     messages:   [ 
                         { 
                             date:   '20/03/2020   16:30:00',
-                            text:   'Ciao   come   stai?',
+                            text:   'Ciao come stai?',
                             status:   'sent'
                         },
                         { 
                             date:   '20/03/2020   16:30:55', 
-                            text:   'Bene   grazie!   Stasera   ci    vediamo?', 
+                            text:   'Bene grazie! Stasera ci vediamo?', 
                             status:   'received' 
                         },   
                         { 
                             date:   '20/03/2020   16:35:00', 
-                            text:   'Mi     piacerebbe   ma    devo   andare   a   fare   la    spesa.', 
+                            text:   'Mi piacerebbe ma devo andare a fare la spesa.', 
                             status:   'sent' 
                         } 
                     ], 
@@ -81,17 +81,17 @@ var app = new Vue (
                     messages:   [ 
                         { 
                             date:   '28/03/2020   10:10:40', 
-                            text:   'La     Marianna   va    in    campagna', 
+                            text:   'La Marianna va in campagna', 
                             status:   'received' 
                         }, 
                         { 
                             date:   '28/03/2020   10:20:10', 
-                            text:   'Sicuro   di    non     aver   sbagliato   chat?', 
+                            text:   'Sicuro di non aver sbagliato chat?', 
                             status:   'sent' 
                         }, 
                         { 
                             date:   '28/03/2020   16:15:22', 
-                            text:   'Ah     scusa!', 
+                            text:   'Ah scusa!', 
                             status:   'received' 
                         } 
                     ], 
@@ -103,12 +103,12 @@ var app = new Vue (
                     messages:   [ 
                         { 
                             date:   '10/01/2020   15:30:55', 
-                            text:   'Lo     sai     che     ha    aperto   una     nuova   pizzeria?', 
+                            text:   'Lo sai che ha aperto una nuova pizzeria?', 
                             status:   'sent' 
                         }, 
                         { 
                             date:   '10/01/2020   15:50:00', 
-                            text:   'Si,   ma    preferirei   andare   al    cinema', 
+                            text:   'Si, ma preferirei andare al cinema', 
                             status:   'received' 
                         } 
                     ], 
